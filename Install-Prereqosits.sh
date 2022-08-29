8b87cbb55e1d34d25c8ce3769a6686ea484971d0
Richard42Graham <up628887@myport.ac.uk>
2022-08-29 16:12:34 +0200

    added a basic install script for setting up on new machines

---
+++ Install-Prereqosits.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt install -Y
+curl
+wget
+feh # (an image viewer)
+mplayer # (a video player)
+termsaver # (print text slowly in terminal)
+fq #  - Command-line JSON processor (json parser for e621 API)
+pv # (for asciiArt)
+lolcat # (for asciiArt)
+mosquitto # ?
+mosquitto-clients #( to recive and hanndle the MQTT messages )
+mpv
