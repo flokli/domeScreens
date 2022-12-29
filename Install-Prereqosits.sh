98f506024d4877f456456bbb6916e8ff6fbe2ff6
Richard42Graham <up628887@myport.ac.uk>
2022-12-29 02:15:05 +0100

    updated install script

--- Install-Prereqosits.sh
+++ Install-Prereqosits.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo apt install -Y
-curl
-wget
-feh # (an image viewer)
-mplayer # (a video player)
-termsaver # (print text slowly in terminal)
-fq #  - Command-line JSON processor (json parser for e621 API)
-pv # (for asciiArt)
-lolcat # (for asciiArt)
-mosquitto # ?
-mosquitto-clients #( to recive and hanndle the MQTT messages )
-mpv
+sudo apt install -Y curl
+sudo apt install -Y wget
+sudo apt install -y feh # (an image viewer)
+sudo apt install -y mplayer # (a video player)
+sudo apt install -y termsaver # (print text slowly in terminal)
+sudo apt install -y fq #  - Command-line JSON processor (json parser for e621 API)
+sudo apt install -y pv # (for asciiArt)
+sudo apt install -y lolcat # (for asciiArt)
+sudo apt install -y mosquitto # ?
+sudo apt install -y mosquitto-clients #( to recive and hanndle the MQTT messages )
+sudo apt install -y mpv
+sudo apt install -y jq
